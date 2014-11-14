8d99cd73b3abda1194ed9af4814728d26d8141f6
James Hrisho <james.hrisho@gmail.com>
2014-11-13 21:39:14 -0500

    use separate commands as arguments

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 awesome-job-dude
 ================
 a little bit of congrats
+
+#Installation
+
 ```
 brew install sox
 npm install -g awesome-job-dude
+````
 
-awesome
+#Use
 
+```
+  Usage: awesome [options]
+
+  Options:
+
+    -h, --help      output usage information
+    -V, --version   output the version number
+    -y, --ytmnd     You the man now dawg
+    -k, --killinit  Killing it
+    -a, --awesome   Awesome
+    --hang          Hang In there
+    -b, --beef      BEEF
+    -o, --otmop     On The Might Of Princes
+    -c, --crushit   Crushing it
 ```
--- index.js
+++ index.js
@@ -3,21 +3,46 @@ var say = require('say'),
  chalk = require('chalk'),
  _ = require('lodash'),
  text = require('./text.json'),
+ program = require('commander'),
  figlet = require('figlet');
+var pjson = require('./package.json');
 
+  program
+    .version(pjson.version)
+    .option('-y, --ytmnd', 'You the man now dawg')
+    .option('-k, --killinit', 'Killing it')
+    .option('-a, --awesome', 'Awesome')
+    .option('--hang', 'Hang In there')
+    .option('-b, --beef', 'BEEF')
+    .option('-o, --otmop', 'On The Might Of Princes')
+    .option('-c, --crushit', 'Crushing it')
+    .parse(process.argv);
 
-var finalText = randomFromList(text);
-figlet(finalText.text, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(chalk.bgGreen(data));
-});
+  var finalText = randomFromList(text);
 
-say.speak('Alex',finalText.voiceText);
+  if (program.ytmnd) finalText  = _.where(text, { 'key': "ytmnd" })[0];
+  if (program.awesome) finalText  = _.where(text, { 'key': "awesome" })[0];
+  if (program.hang) finalText  = _.where(text, { 'key': "hang" })[0];
+  if (program.killinit) finalText  = _.where(text, { 'key': "killinit" })[0];
+  if (program.beef) finalText  = _.where(text, { 'key': "beef" })[0];
+  if (program.otmop) finalText  = _.where(text, { 'key': "otmop" })[0];
+  if (program.crushit) finalText  = _.where(text, { 'key': "crushingit" })[0];
 
-function randomFromList(list) {
-  return list[_.random(list.length - 1)];
-}
+  execute(finalText);
+
+  function execute(finalText) {
+    figlet(finalText.text, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(chalk.bgGreen(data));
+    });
+
+    say.speak('Alex',finalText.voiceText); 
+  }
+
+  function randomFromList(list) {
+    return list[_.random(list.length - 1)];
+  }
